669e1174b3774a5531deb7e497902ed6ef20e0e8
Guilherme <Guilherme@DESKTOP-BBFH37V>
2016-11-25 01:47:30 -0200

    November 25th Trending Topics

--- src/core/Main.java
+++ src/core/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
 	public static void main(String[] args) throws IOException, TwitterException {
 		//TrendingTopicSearch.searchTrends(TrendingTopicSearch.UNLIMITED, 1);
-		TrendingTopicSearch.searchTrends(5, 5);
+		TrendingTopicSearch.searchTrends(TrendingTopicSearch.UNLIMITED, 30);
 	}
 
 }
